e37f2e6b3efc984ffeaee0b95fccb5a76ce5ed1a
DELL <DELL@DESKTOP-H15KJTI>
2025-09-10 11:11:33 +0530

    intial commit

---
+++ TCF_Training/src/com/tcf/mvit/abstractionassignment/MainSimulation.java
@@ -0,0 +1,22 @@
+package com.tcf.mvit.abstractionassignment;
+
+public class MainSimulation {
+    public static void main(String[] args) {
+        // Self Driving Car
+        SelfDrivingCar car = new SelfDrivingCar("CAR123", 40, new double[]{12.5, 45.7}, "YOLO-LaneNet", 0.8);
+        car.navigate();
+        car.adjustSpeed(60);
+        car.detectObstacles();
+        car.logTelemetry();
+
+        System.out.println();
+
+        // Delivery Drone
+        DeliveryDrone drone = new DeliveryDrone("DRONE007", 30, new double[]{5.2, 18.4}, 120.5, 2.3);
+        drone.navigate();
+        drone.adjustSpeed(50);
+        drone.detectObstacles();
+        drone.logTelemetry();
+    }
+}
+
---
+++ TCF_Training/src/com/tcf/mvit/abstractionassignment/VehicleAI.java
@@ -0,0 +1,28 @@
+package com.tcf.mvit.abstractionassignment;
+
+
+
+//Abstract Class
+public abstract class VehicleAI {
+ protected String vehicleID;
+ protected double currentSpeed;
+ protected double[] locationCoordinates; // [x, y] position
+
+ public VehicleAI(String vehicleID, double currentSpeed, double[] locationCoordinates) {
+     this.vehicleID = vehicleID;
+     this.currentSpeed = currentSpeed;
+     this.locationCoordinates = locationCoordinates;
+ }
+
+ // Abstract methods
+ public abstract void navigate();
+ public abstract void adjustSpeed(double targetSpeed);
+ public abstract void detectObstacles();
+
+ // Concrete method
+ public void logTelemetry() {
+     System.out.println("Telemetry Log -> ID: " + vehicleID +
+             ", Speed: " + currentSpeed +
+             " km/h, Location: (" + locationCoordinates[0] + ", " + locationCoordinates[1] + ")");
+ }
+}
